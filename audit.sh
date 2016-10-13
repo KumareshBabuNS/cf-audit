f2e395cd7777271a9ff191f99034f30c4533e20b
James Abley <james.abley@gmail.com>
2016-10-13 10:32:26 +0100

    Make it work on Bluemix
    See https://developer.ibm.com/answers/questions/191474/cf-org-users-and-users-name.html
    
    Bluemix doesn’t work with vanilla `cf` at the moment. IBM support claim
    that you need admin rights to convert a user GUID into a username.
    
    No other Cloud Foundry that I’ve tried (PWS, Swisscom) seems to need
    this.
    
    Using the `bx` tool using the same CF login means that you do get the
    usernames. ¯\_(ツ)_/¯
    
    Also add graceful failure if `cf`, `jq`, or `bx` are needed, but aren’t
    installed.

--- audit.sh
+++ audit.sh
@@ -12,12 +12,53 @@ ORG - the organization name in Cloud Foundry
   exit 1
 fi
 
+command -v cf >/dev/null 2>&1 || {
+  cat >&2 << EOM
+I require cf (the Cloud Foundry CLI tool).
+
+> brew cask install cf-cli will fix this on MacOS.
+
+Aborting
+EOM
+  exit 1;
+}
+
+command -v cf >/dev/null 2>&1 || {
+  cat >&2 << EOM
+I require jq (JSON query CLI tool).
+
+> brew cask install jq will fix this on MacOS.
+
+Aborting
+EOM
+  exit 1;
+}
+
 ORG_GUID=$(cf curl "/v2/organizations" | \
 jq -r '.resources[] | select(.entity.name == "'$ORG'") | .metadata.guid')
 
 # Get the ORG USERS
-cf org-users $ORG
+if [ $(cf api | grep -c bluemix) -eq "1" ]; then
+  # Bluemix is stupid and doesn't behave like any other Cloud Foundry we're using
+  # See https://developer.ibm.com/answers/questions/191474/cf-org-users-and-users-name.html
+  command -v bx >/dev/null 2>&1 || {
+    cat >&2 << EOM
+I require bx (Bluemix CLI tool)
 
+> brew cask install bluemix-cli will fix this on MacOS.
+
+Aborting
+EOM
+    exit 1;
+  }
+  ORG_USERS_CMD="bx iam org-users"
+  SPACE_USERS_CMD="bx iam space-users"
+else
+  ORG_USERS_CMD="cf org-users"
+  SPACE_USERS_CMD="cf space-users"
+fi
+
+${ORG_USERS_CMD} $ORG
 echo
 echo
 
@@ -27,7 +68,7 @@ jq -r '.resources[].entity.name')
 
 # For each space, get the Space users
 for SPACE in $SPACES; do
-  cf space-users $ORG $SPACE
+  ${SPACE_USERS_CMD} $ORG $SPACE
   echo
   echo
 done
